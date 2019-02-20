e711e8ab974f45d78f021ff689001f30498e25e5
OladapoAjala <ajalaoladapoemmanuel.ao@gmail.com>
2019-02-20 11:13:51 +0100

    Adjusted inventoneDistanceMeasuringSystem_LCD

--- inventoneDistanceMeasuring_WebInterface/inventoneDistanceMeasuring_WebInterface.ino
+++ inventoneDistanceMeasuring_WebInterface/inventoneDistanceMeasuring_WebInterface.ino
@@ -1,44 +1,47 @@
 /*
- * Temperature Monitoring Web Server Connects to your Router
- * circuits4you.com
+ * Distance measuring using HCSR04 lib
+ * 
  */
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
-#include "DHT.h"
+#include <HCSR04.h>
 #include "index.h"  //Web page
 
-#define DHTPIN 12     // what digital pin we're connected to (D8)
-#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+#define TRIG_PIN 13
+#define ECHO_PIN 12
+
+HCSR04 hc(TRIG_PIN, ECHO_PIN);   //initialisation class HCSR04 (trig pin , echo pin)
 
 //SSID and Password of your WiFi router
 const char* ssid = "ESP8266_WIFI";
 const char* password = "123456789";
 
 ESP8266WebServer server(80); //Server on port 80
-DHT dht(DHTPIN, DHTTYPE);
 
 //===============================================================
 //     This routine is executed when you open its IP in browser
 //===============================================================
+
 void handleRoot() {
   String s = MAIN_page; 
-  int Temperature; 
-  Temperature = (int)dht.readTemperature();  //Read Analog Voltage of ADC Pin
-  Serial.print("Temperature is: ");
-  Serial.println(Temperature);
+  int Distance; 
+  Distance = hc.dist();  //Read distance from ultrasonic sensor
+  Serial.print("Distance is: ");
+  Serial.println(Distance);
   //Convert Temperature int to String then Replace @@temp@@ in HTML with temperaure value
-  s.replace("@@temp@@",String(Temperature));  
+  s.replace("@@temp@@",String(Distance));  
   server.send(200, "text/html", s); //Send webpage to browser
 }
 
 //===============================================================
 //                  SETUP
 //===============================================================
+
 void setup(void){
-  Serial.begin(9600);
-  dht.begin();
   WiFi.begin(ssid, password);     //Connect to your WiFi router
+  WiFi.mode(WIFI_STA); //WiFi mode station (connect to wifi router only
+  Serial.begin(9600);
   Serial.println("");
 
   // Wait for connection
@@ -59,9 +62,11 @@ void setup(void){
   server.begin();                  //Start server
   Serial.println("HTTP server started");
 }
+
 //===============================================================
 //                     LOOP
 //===============================================================
+
 void loop(void){
   server.handleClient();          //Handle client requests
 }
--- inventoneTemperatureMeasuring_WebInterface/inventoneTemperatureMeasuring_WebInterface.ino
+++ inventoneTemperatureMeasuring_WebInterface/inventoneTemperatureMeasuring_WebInterface.ino
@@ -1,47 +1,44 @@
 /*
- * Distance measuring using HCSR04 lib
- * 
+ * Temperature Monitoring Web Server Connects to your Router
+ * circuits4you.com
  */
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
-#include <HCSR04.h>
+#include "DHT.h"
 #include "index.h"  //Web page
 
-#define TRIG_PIN 13
-#define ECHO_PIN 12
-
-HCSR04 hc(TRIG_PIN, ECHO_PIN);   //initialisation class HCSR04 (trig pin , echo pin)
+#define DHTPIN 12     // what digital pin we're connected to (D8)
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 
 //SSID and Password of your WiFi router
 const char* ssid = "ESP8266_WIFI";
 const char* password = "123456789";
 
 ESP8266WebServer server(80); //Server on port 80
+DHT dht(DHTPIN, DHTTYPE);
 
 //===============================================================
 //     This routine is executed when you open its IP in browser
 //===============================================================
-
 void handleRoot() {
   String s = MAIN_page; 
-  int Distance; 
-  Distance = hc.dist();  //Read distance from ultrasonic sensor
-  Serial.print("Distance is: ");
-  Serial.println(Distance);
+  int Temperature; 
+  Temperature = (int)dht.readTemperature();  //Read Analog Voltage of ADC Pin
+  Serial.print("Temperature is: ");
+  Serial.println(Temperature);
   //Convert Temperature int to String then Replace @@temp@@ in HTML with temperaure value
-  s.replace("@@temp@@",String(Distance));  
+  s.replace("@@temp@@",String(Temperature));  
   server.send(200, "text/html", s); //Send webpage to browser
 }
 
 //===============================================================
 //                  SETUP
 //===============================================================
-
 void setup(void){
-  WiFi.begin(ssid, password);     //Connect to your WiFi router
-  WiFi.mode(WIFI_STA); //WiFi mode station (connect to wifi router only
   Serial.begin(9600);
+  dht.begin();
+  WiFi.begin(ssid, password);     //Connect to your WiFi router
   Serial.println("");
 
   // Wait for connection
@@ -62,11 +59,9 @@ void setup(void){
   server.begin();                  //Start server
   Serial.println("HTTP server started");
 }
-
 //===============================================================
 //                     LOOP
 //===============================================================
-
 void loop(void){
   server.handleClient();          //Handle client requests
 }
