1fb57283b51e3c2811da69c6af77f287e2b8c6a5
OladapoAjala <ajalaoladapoemmanuel.ao@gmail.com>
2019-03-28 17:36:04 +0100

    Added the New IoT light with awesome web interface

---
+++ inventoneIoTLight/inventoneIoTLight.ino
@@ -0,0 +1,161 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <DNSServer.h>
+#include <FS.h>
+#include <ESP8266HTTPUpdateServer.h>
+
+#define BULB 12
+#define DBG_OUTPUT_PORT Serial
+
+const byte DNS_PORT = 53;
+IPAddress apIP(192, 168, 2, 1);
+DNSServer dnsServer;
+ESP8266WebServer server(80);
+ESP8266HTTPUpdateServer httpUpdater;
+
+//holds the current upload
+File fsUploadFile;
+
+//format bytes
+String formatBytes(size_t bytes) {
+  if (bytes < 1024) {
+    return String(bytes) + "B";
+  } else if (bytes < (1024 * 1024)) {
+    return String(bytes / 1024.0) + "KB";
+  } else if (bytes < (1024 * 1024 * 1024)) {
+    return String(bytes / 1024.0 / 1024.0) + "MB";
+  } else {
+    return String(bytes / 1024.0 / 1024.0 / 1024.0) + "GB";
+  }
+}
+
+String getContentType(String filename) {
+  if (server.hasArg("download")) {
+    return "application/octet-stream";
+  } else if (filename.endsWith(".htm")) {
+    return "text/html";
+  } else if (filename.endsWith(".html")) {
+    return "text/html";
+  } else if (filename.endsWith(".css")) {
+    return "text/css";
+  } else if (filename.endsWith(".js")) {
+    return "application/javascript";
+  } else if (filename.endsWith(".png")) {
+    return "image/png";
+  } else if (filename.endsWith(".gif")) {
+    return "image/gif";
+  } else if (filename.endsWith(".jpg")) {
+    return "image/jpeg";
+  } else if (filename.endsWith(".ico")) {
+    return "image/x-icon";
+  } else if (filename.endsWith(".xml")) {
+    return "text/xml";
+  } else if (filename.endsWith(".pdf")) {
+    return "application/x-pdf";
+  } else if (filename.endsWith(".zip")) {
+    return "application/x-zip";
+  } else if (filename.endsWith(".gz")) {
+    return "application/x-gzip";
+  }
+  return "text/plain";
+}
+
+bool handleFileRead(String path) {
+  DBG_OUTPUT_PORT.println("handleFileRead: " + path);
+  if (path.endsWith("/")) {
+    path += "index.html";
+  }
+  String contentType = getContentType(path);
+  String pathWithGz = path + ".gz";
+  if (SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)) {
+    if (SPIFFS.exists(pathWithGz)) {
+      path += ".gz";
+    }
+    File file = SPIFFS.open(path, "r");
+    server.streamFile(file, contentType);
+    file.close();
+    return true;
+  }
+  return false;
+}
+
+boolean setupAP() {
+  WiFi.disconnect();
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+  WiFi.softAP("ina", "inventech");
+  Serial.println("");
+  Serial.print("IP: ");
+  Serial.println(WiFi.softAPIP());
+  Serial.println("");
+  return true;
+}
+
+boolean handleON() {
+  Serial.println("Turning light on");
+  digitalWrite(BULB, HIGH);
+  return true;
+}
+
+boolean handleOFF() {
+  Serial.println("Turning light off");
+  digitalWrite(BULB, LOW);
+  return true;
+}
+
+
+boolean handleUpdate() {
+  server.send(200, "text/plain", "OTA Update Enabled !");
+  Serial.println("OTA Update");
+}
+
+
+void setup(void) {
+  DBG_OUTPUT_PORT.begin(115200);
+  DBG_OUTPUT_PORT.print("\n");
+  DBG_OUTPUT_PORT.setDebugOutput(true);
+  
+  pinMode(BULB, OUTPUT);
+  setupAP();
+  
+  SPIFFS.begin();
+  {
+    Dir dir = SPIFFS.openDir("/");
+    while (dir.next()) {
+      String fileName = dir.fileName();
+      size_t fileSize = dir.fileSize();
+      DBG_OUTPUT_PORT.printf("FS File: %s, size: %s\n", fileName.c_str(), formatBytes(fileSize).c_str());
+    }
+    DBG_OUTPUT_PORT.printf("\n");
+  }
+  
+  
+  {
+  dnsServer.setTTL(300);
+  dnsServer.setErrorReplyCode(DNSReplyCode::ServerFailure);
+  dnsServer.start(DNS_PORT, "www.light.io", apIP);
+  
+  server.on("/on", HTTP_GET, handleON);
+  server.on("/off", HTTP_GET, handleOFF);
+  //server.on("/update", HTTP_GET, handleUpdate);
+  }
+  //use it to load content from SPIFFS
+  server.onNotFound([]() {
+    Serial.print("URL: ");
+    Serial.println(server.uri());
+    if (!handleFileRead(server.uri())) {
+      server.send(404, "text/plain", "FileNotFound");
+    }
+  });
+
+  httpUpdater.setup(&server);
+  server.begin();
+  DBG_OUTPUT_PORT.println("HTTP server started");
+
+}
+
+void loop(void) {
+  server.handleClient();
+  dnsServer.processNextRequest();
+}
